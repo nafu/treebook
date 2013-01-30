d7e3007ec408ea660df4c11ad4a6e027cc6c9006
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2013-01-30 01:45:44 -0800

    Fixed bug with user profile validation

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -10,17 +10,20 @@ class UserTest < ActiveSupport::TestCase
   	assert !user.errors[:first_name].empty?
   end
 
+
   test "a user should enter a last name" do
   	user = User.new
   	assert !user.save
   	assert !user.errors[:last_name].empty?
   end
 
+
   test "a user should enter a profile name" do
   	user = User.new
   	assert !user.save
   	assert !user.errors[:profile_name].empty?
   end
+
 
   test "a user should have a unique profile name" do
   	user = User.new
@@ -29,12 +32,24 @@ class UserTest < ActiveSupport::TestCase
   	assert !user.errors[:profile_name].empty?
   end
 
+
   test "a user should have a profile name without spaces" do
-  	user = User.new
-  	user.profile_name = "My Profile with Spaces"
+    user = User.new(first_name: 'Jason', last_name: 'Seifer', email: 'jason2@teamtreehouse.com')
+    user.password = user.password_confirmation = 'asdfasdf'
+
+    user.profile_name = "My Profile with Spaces"
 
   	assert !user.save
   	assert !user.errors[:profile_name].empty?
   	assert user.errors[:profile_name].include?("Must be formatted correctly.")
+  end
+
+
+  test "a user can have a correctly formatted profile name" do
+    user = User.new(first_name: 'Jason', last_name: 'Seifer', email: 'jason2@teamtreehouse.com')
+    user.password = user.password_confirmation = 'asdfasdf'
+
+    user.profile_name = 'jasonseifer_1'
+    assert user.valid?
   end
 end
