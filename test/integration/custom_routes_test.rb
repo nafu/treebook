6d0e70b5d633ef158f8209c9b6233513c5ff0552
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2013-01-30 17:21:22 -0800

    Updated profile and added scoped status updates to it

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -19,4 +19,10 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
   	get '/register'
   	assert_response :success
   end
+
+  test "that a profile page works" do
+    get '/jasonseifer'
+    assert_response :success
+  end
+
 end
