c5a625a4902c42766ac3ee04dc30a89b136f6f47
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2013-02-10 22:07:51 -0800

    Fixed routes for devise user

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Treebook::Application.routes.draw do
   devise_for :users
 
   devise_scope :user do
-    get "register", :to => "devise/registrations#new", as: :register
-    get "login", :to => "devise/sessions#new", as: :login
-    get "logout", :to => "devise/sessions#destroy", as: :logout
+    get "/register", :to => "devise/registrations#new", as: :register
+    get "/login", :to => "devise/sessions#new", as: :login
+    get "/logout", :to => "devise/sessions#destroy", as: :logout
   end
 
   resources :user_friendships
