86086899dbfb26e455e925a78080fadcb3425f92
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2013-02-26 12:34:28 -0800

    Added index for user_friendships

--- app/controllers/user_friendships_controller.rb
+++ app/controllers/user_friendships_controller.rb
@@ -1,5 +1,8 @@
 class UserFriendshipsController < ApplicationController
-	before_filter :authenticate_user!, only: [:new]
+	before_filter :authenticate_user!
+
+	def index
+	end
 
 	def new
 	  if params[:friend_id]
--- test/functional/user_friendships_controller_test.rb
+++ test/functional/user_friendships_controller_test.rb
@@ -1,6 +1,29 @@
 require 'test_helper'
 
 class UserFriendshipsControllerTest < ActionController::TestCase
+  context "#index" do
+    context "when not logged in" do
+      should "redirect to the login page" do
+        get :index
+        assert_response :redirect
+      end
+    end
+
+    context "when logged in" do
+      setup do
+        @friendship1 = create(:pending_user_friendship, user: users(:jason), friend: create(:user, first_name: 'Pending', last_name: 'Friend'))
+        @friendship2 = create(:accepted_user_friendship, user: users(:jason), friend: create(:user, first_name: 'Active', last_name: 'Friend'))
+
+        sign_in users(:jason)
+        get :index
+      end
+
+      should "get the index page without error" do
+        assert_response :success
+      end
+    end
+  end
+
   context "#new" do
   	context "when not logged in" do
   		should "redirect to the login page" do
